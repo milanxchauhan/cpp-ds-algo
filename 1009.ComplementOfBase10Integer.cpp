15b6bd2d1f21ad3a02a4f2cebec312132c5847ed
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-23 10:46:00 +0530

    Problem Number - 1009

---
+++ 1009.ComplementOfBase10Integer.cpp
@@ -0,0 +1,25 @@
+// Problem - https://leetcode.com/problems/complement-of-base-10-integer/
+// Runtime: 2 ms
+// Memory Usage: 5.98 MB
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        
+        int m = n;
+        int mask = 0;
+
+        if(n == 0){
+            return 1;
+        }
+
+        while(m!=0){
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+
+        int ans = (~n) & mask;
+
+        return ans;
+    }
+};
