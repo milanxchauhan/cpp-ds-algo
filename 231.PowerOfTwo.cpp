491a4878ccc6f8bfdafd48dac0e7d283cef793d3
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-24 21:54:23 +0530

    Problem Number - 231

---
+++ 231.PowerOfTwo.cpp
@@ -0,0 +1,19 @@
+// Problem - https://leetcode.com/problems/power-of-two/
+// Runtime: 0 ms
+// Memory Usage: 5.96 MB
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans = 1;
+        for(int i=0; i<=30; i++){
+            if(ans == n){
+                return true;
+            }
+            if(ans < INT_MAX/2){
+                ans = ans*2;
+            }          
+        }
+        return false;
+    }
+};
