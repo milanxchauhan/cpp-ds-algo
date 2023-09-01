436faf01cd124689fef893bb54ef97f0b186f801
Milan Chauhan <milanchauhan.development@gmail.com>
2023-09-01 18:40:00 +0530

    Problem Number - 15

---
+++ 15.3Sum.cpp
@@ -0,0 +1,42 @@
+// Problem - https://leetcode.com/problems/3sum/
+// Runtime: 102 ms
+// Memory Usage: 23.93 MB
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int start = i + 1;
+            int end = nums.size() - 1;
+
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[start], nums[end]});
+
+                    while (start < end && nums[start] == nums[start + 1])
+                        start++;
+                    while (start < end && nums[end] == nums[end - 1])
+                        end--;
+
+                    start++;
+                    end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
