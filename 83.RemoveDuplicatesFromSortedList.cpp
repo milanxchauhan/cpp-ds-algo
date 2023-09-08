a99bba59beddfef8eab239edfc3e52bbf766b3fd
Milan Chauhan <milanchauhan.development@gmail.com>
2023-09-08 06:19:45 +0530

    Problem Number - 83

---
+++ 83.RemoveDuplicatesFromSortedList.cpp
@@ -0,0 +1,35 @@
+// Problem - https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+// Runtime: 8 ms
+// Memory Usage: 11.59 MB
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr; // Empty list
+        }
+        
+        ListNode* current = head;
+        while (current->next != nullptr) {
+            if (current->val == current->next->val) {
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+            } else {
+                current = current->next;
+            }
+        }
+        
+        return head;
+    }
+};
