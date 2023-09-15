e09335e28e3dba468d55ccb28b4dcd41eadb2dd3
Milan Chauhan <milanchauhan.development@gmail.com>
2023-09-15 09:12:34 +0530

    Problem Number - 225

---
+++ 225.ImplementStackUsingQueues.cpp
@@ -0,0 +1,48 @@
+// Problem - https://leetcode.com/problems/implement-stack-using-queues/
+// Runtime: 0 ms
+// Memory Usage: 7.14 MB
+
+#include <queue>
+
+class MyStack {
+private:
+    std::queue<int> q1;
+    std::queue<int> q2;
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        std::swap(q1, q2);
+    }
+    
+    int pop() {
+        int topElement = q1.front();
+        q1.pop();
+        return topElement;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
